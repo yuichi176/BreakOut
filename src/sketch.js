3186322f0eac3a14d967540b4155e077a653ea37
Yuichi Sugiyama <ozwald176@gmail.com>
2021-09-16 16:15:30 +0900

    version 1.0.1

--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,17 @@
 //breakout close (core mechanics)
 //mouse to control the paddle, click to start
 
+// 変数宣言
 // 画像
-let heart1, heart2;
+let heart1, heart2, sound_on, sound_off;
+// 音声
+let effect_sound1, effect_sound2;
 // スプライト
 let paddle, ball, wallTop, wallBottom, wallLeft, wallRight, sound;
-// ゲームの状態を表す変数("play" or "stay")
+// ゲームの状態を表す変数("start",stay","play","over","finish")
 let gameState = "start";
 // 残りライフ保持する変数
 let life_count = 3;
-// 固定値
-var FIRST_SPEED = 9;
-var MAX_SPEED = 12;
-var WALL_THICKNESS = 30;
-var BRICK_W = 100;
-var BRICK_H = 25;
-var BRICK_MARGIN = 6;
-var ROWS = 7;
-var COLUMNS = 6;
-var GAME_OVER_COUNT = 3;
-
 // タイマー関連
 // 1秒
 let oneSec = 1000;
@@ -29,6 +21,18 @@ let startTime;
 let elapsedTime = 0;
 // 秒数をカウント
 let count = 0;
+
+// 固定値
+const FIRST_SPEED = 9;
+const MAX_SPEED = 12;
+const WALL_THICKNESS = 30;
+const BRICK_W = 100;
+const BRICK_H = 25;
+const BRICK_MARGIN = 6;
+const ROWS = 7;
+const COLUMNS = 6;
+const GAME_OVER_COUNT = 3;
+
 
 // 画像ファイル、音声ファイルの読み込み
 function preload() {
@@ -68,8 +72,8 @@ function setup() {
   // ブロックのスプライト (グループ)
   bricks = new Group();
 
-  var offsetX = width/2-(COLUMNS-1)*(BRICK_MARGIN+BRICK_W)/2;
-  var offsetY = 80;
+  let offsetX = width/2-(COLUMNS-1)*(BRICK_MARGIN+BRICK_W)/2;
+  let offsetY = 80;
 
   for(var r = 0; r<ROWS; r++)
     for(var c = 0; c<COLUMNS; c++) {
@@ -84,10 +88,10 @@ function setup() {
   ball.maxSpeed = MAX_SPEED;
   paddle.shapeColor = ball.shapeColor = color(255, 255, 255);
 
-  // ライフのスプライト
+  // ライフのスプライト (グループ)
   lifes = new Group;
 
-  var offsetX_life = 40;
+  let offsetX_life = 40;
 
   for (let i = 0; i<3; i++) {
     let life = createSprite((width-25)-offsetX_life*i, 25, heart1.width, heart1.height);
@@ -102,6 +106,13 @@ function setup() {
   sound.addAnimation("on",sound_on);
   sound.mouseActivate = true;
 
+  sound.onMouseOver = () => {
+    cursor(HAND);
+  }
+  sound.onMouseOut = () => {
+    cursor(ARROW);
+  }
+
   sound.onMousePressed = () => {
     if( sound.getAnimationLabel() == "off"){
       sound.changeAnimation("on");
@@ -109,13 +120,6 @@ function setup() {
       sound.changeAnimation("off");
     }
   }
-
-  sound.onMouseOver = () => {
-      cursor(HAND);
-  }
-  sound.onMouseOut = () => {
-    cursor(ARROW);
-}
 
 }
 
@@ -132,6 +136,8 @@ function update() {
   if( bricks.length == 0) { gameFinish(); }
 
   if (gameState == "start") {  // gameState = start の時
+
+    // テキスト表示
     fill(255);
     textAlign(CENTER);
     textSize(13);
@@ -140,6 +146,7 @@ function update() {
 
   } else if (gameState == "stay") {  // gameState = stay の時
 
+    // テキスト表示
     fill(255);
     textAlign(CENTER);
     textSize(13);
@@ -147,33 +154,23 @@ function update() {
     paddle.position.x = constrain(mouseX, paddle.width/2, width-paddle.width/2);
 
     // タイマー処理
+    timeCount();
     fill(30);
     textAlign(CENTER);
     textSize(18);
     textStyle(BOLD);
     text(count, 25, 30);
-    // 現在時間値
-    let now = millis();
-    // 経過時間値
-    elapsedTime = now - startTime;
-    // 1秒たったら
-    if (elapsedTime >= oneSec) {
-      // 秒数を1つ大きくする
-      count++;
-      // 再びスタート
-      startTime = millis();
-    }
 
   } else if (gameState == "play") {  // gameState = play の時
 
     paddle.position.x = constrain(mouseX, paddle.width/2, width-paddle.width/2);
 
-    // ボールが壁に当たったら跳ね返す
+    // ボールが壁で跳ね返った時の処理
     ball.bounce(wallTop);
     ball.bounce(wallLeft);
     ball.bounce(wallRight);
 
-    // ボールがバーに当たった時の処理
+    // ボールがバーで跳ね返った時の処理
     if(ball.bounce(paddle)) {
       let swing = (ball.position.x-paddle.position.x)/3;
       ball.setSpeed(ball.getSpeed(), ball.getDirection()+swing);
@@ -183,49 +180,26 @@ function update() {
       }
     }
 
-    // ボールがブロックに当たって跳ね返った時の処理
+    // ボールがブロックで跳ね返った時の処理
     ball.bounce(bricks, brickHit);
 
     // ボールが落ちた時の処理
-    if(ball.overlap(wallBottom)) {
-      // ボールを初期位置に戻す
-      ball.position.x = width/2;
-      ball.position.y = height-200;
-      ball.setSpeed(0);
-      // ライフを減らす
-      life_count -= 1;
-      // ライフの画像を変更
-      lifes[life_count].changeAnimation("die");
-      // ゲームの状態を "stay" に変更
-      gameState = "stay";
-      // 落ちた回数を評価
-      if (life_count == 0) { gameOver(); }
-    }
+    if(ball.overlap(wallBottom)) { ballFall(); }
 
     // タイマー処理
+    timeCount();
     fill(30);
     textAlign(CENTER);
     textSize(18);
     textStyle(BOLD);
     text(count, 25, 30);
-    // 現在時間値
-    let now = millis();
-    // 経過時間値
-    elapsedTime = now - startTime;
-    // 1秒たったら
-    if (elapsedTime >= oneSec) {
-      // 秒数を1つ大きくする
-      count++;
-      // 再びスタート
-      startTime = millis();
-    }
 
   } else if (gameState == "over") {  // gameState = over の時
     // 背景色の変更
     noStroke();
     fill(0, 180, 180);
     rect(0, 0, width, height);
-    // 文字列の表示
+    // テキスト表示
     fill(0);
     textAlign(CENTER);
     textSize(25);
@@ -236,13 +210,13 @@ function update() {
     paddle.position.x = constrain(mouseX, paddle.width/2, width-paddle.width/2);
 
   } else if (gameState == "finish") { // gameState = finish の時
-    // 文字列の表示
+    // テキスト表示
     fill(255);
     textAlign(CENTER);
     textSize(50);
     text("Congratulation!", width/2, height/2-45);
     textSize(30);
-    text("クリアタイム:" + count + "秒", width/2, height/2+15);
+    text("クリアタイム:" + count + "秒", width/2, height/2+10);
     textSize(15);
     text("クリックでもう一度", width/2, height/2+60);
 
@@ -250,6 +224,7 @@ function update() {
   }
 }
 
+// 関数
 function mousePressed() {
   if( sound.mouseIsOver == true ) {  //マウスがサウンド切り替えボタン常にある時は何もしない
     ;
@@ -259,9 +234,9 @@ function mousePressed() {
       newGame(); 
     } else if( gameState == "start" || gameState == "stay" ) { // ボールが初期位置にある状態でクリックされた時の処理
       ball.setSpeed(FIRST_SPEED, random(90-10, 90+10));
-      gameState = "play";
       // 開始時間値を取得
       startTime = millis();
+      gameState = "play";
     }
   }
 
@@ -275,18 +250,33 @@ function brickHit(ball, brick) {
   brick.remove();
 }
 
+function ballFall() {
+  // ボールを初期位置に戻す
+  ball.position.x = width/2;
+  ball.position.y = height-200;
+  ball.setSpeed(0);
+  // ライフを減らす
+  life_count -= 1;
+  // ライフの画像を変更
+  lifes[life_count].changeAnimation("die");
+  // ゲームの状態を "stay" に変更
+  gameState = "stay";
+  // 落ちた回数を評価
+  if (life_count == 0) { gameOver(); }
+}
+
 function gameOver() {
   updateSprites(false);
-  gameState = "over";
   // ボールを消す
   ball.remove();
+  gameState = "over";
 }
 
 function gameFinish() {
   updateSprites(false);
-  gameState = "finish";
   // ボールを消す
   ball.remove();
+  gameState = "finish";
 }
 
 function newGame() {
@@ -294,7 +284,7 @@ function newGame() {
   bricks.removeSprites();
   updateSprites(true);
 
-  // ボールを新たに生成
+  // ボールのスプライトを再生成
   ball = createSprite(width/2, height-200, 13, 13);
   ball.maxSpeed = MAX_SPEED;
   paddle.shapeColor = ball.shapeColor = color(255, 255, 255);
@@ -322,4 +312,18 @@ function newGame() {
   count = 0;
 
   gameState = "start";
+}
+
+function timeCount() {
+  // 現在時間値
+  let now = millis();
+  // 経過時間値
+  elapsedTime = now - startTime;
+  // 1秒たったら
+  if (elapsedTime >= oneSec) {
+    // 秒数を1つ大きくする
+    count++;
+    // 再びスタート
+    startTime = millis();
+  }
 }
